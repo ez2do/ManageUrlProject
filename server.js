c8ed7d5bc7bbc4b992e76b20766552f460282105
TuanAnh <tuananh2431998@gmail.com>
2019-04-23 16:07:09 +0700

    add some more handler

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const metascraper = require('metascraper')([
     require('metascraper-publisher')(),
     require('metascraper-title')(),
     require('metascraper-url')()
-])
+]);
+const utils = require('./utils/server_utils');
 
 dotenv.config();
 
@@ -30,203 +31,49 @@ app.use(bodyParser.json());
 
 //get all links
 app.get('/links', (req, res) => {
-    pool.query(
-        `SELECT * FROM url_info`
-    ).then((result) => {
-        res.send({
-            rowCount: result.rowCount,
-            rows: result.rows
-        });
-    }).catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    });
+    utils.getAll('url_info', res);
 });
 
 //add new link
 app.post('/links', (req, res) => {
     var targetUrl = req.body.url;
-    (async () => {
-        try {
-            //get information from url
-            var link = await got(targetUrl);
-            var link_info = await metascraper({ html: link.body, url: link.url });
-            //get information from domain
-            var domain = await got(extractDomain(targetUrl));
-            var domain_info = await metascraper({ html: domain.body, url: domain.url });
-
-            //add domain to database
-            await pool.query({
-                text: `SELECT * FROM domain_info WHERE name = $1`,
-                values: [domain_info.url]
-            }).then((result) => {
-                if (result.rowCount !== 0) {
-                    console.log('Domain already in the database');
-                    return false;   //stop insert
-                }
-                return true;   //insert
-            }).then((result) => {
-                if (result) {
-                    pool.query({
-                        text: `INSERT INTO 
-                        domain_info(name, title, logo_link, img_link, description, publisher)
-                        VALUES($1, $2, $3, $4, $5, $6)`,
-                        values: [domain_info.url, domain_info.title, domain_info.logo, domain_info.image,
-                        domain_info.description, domain_info.publisher]
-                    });
-                }
-            }).catch((err) => {
-                console.log('Fail to insert domain\n', err);
-            });
-
-            //get id of domain
-            var domain_id;
-            await pool.query({
-                text: `SELECT id FROM domain_info WHERE name = $1`,
-                values: [domain_info.url]
-            }).then((result) => {
-                domain_id = result.rows[0].id;
-            }).catch((err) => {
-                console.log('Can not get domain id\n', err);
-            })
-
-            //check whether url already exists in the database
-            await pool.query({
-                text: `SELECT * FROM url_info WHERE url = $1`,
-                values: [link.url]
-            }).then((result) => {
-                if (result.rowCount !== 0) {
-                    console.log('Url exists in the database');
-                    res.send({
-                        success: true,
-                        exists: true
-                    });
-                    return false;   //stop insert
-                }
-                return true;    //do insert into table
-            }).then((result) => {
-                if (result) {
-                    pool.query({
-                        text: `INSERT INTO 
-                                url_info(url, title, logo_link, img_link, description, publisher, collection_id, domain_id)
-                                VALUES($1, $2, $3, $4, $5, $6, $7, $8)`,
-                        values: [link.url, link_info.title, link_info.logo, link_info.image, link_info.description,
-                        link_info.publisher, 1, domain_id]
-                    }).then(() => {
-                        res.send(link_info);
-                    }).catch((err) => {
-                        res.send({ success: false, error: err });
-                    });
-                }
-            }).catch((err) => {
-                return res.send({ success: false, error: err });
-            });
-        } catch (err) {
-            console.log(err);
-            return res.send({ success: false, error: err });
-        }
-    })()
+    utils.postLink(targetUrl, 'domain_info', 'url_info', res);
 });
 
 //info about 1 link
 app.get('/links/:id', (req, res) => {
     var id = req.params.id;
-    pool.query({
-        text: `SELECT * FROM url_info WHERE id = $1`,
-        values: [id]
-    }).then((result) => {
-        res.send({
-            rowCount: result.rowCount,
-            rows: result.rows
-        });
-    }).catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    });
-});
-
-//insert or remove 1 link from a collection by change property "is_favorite"
-app.put('/links/:id', (req, res) => {
-
+    utils.getById('url_info', id, res);
 });
 
 //delete 1 link
 app.delete('/links/:id', (req, res) => {
-    pool.query({
-        text: `DELETE FROM url_info WHERE id = ${req.params.id}`
-    }).then((result) => {
-        res.send({
-            success: true,
-            result: result
-        });
-    }).catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    });
+    var id = req.params.id;
+    utils.deleteById('url_info', id, res);
 });
 
 //add 1 link to collection
 app.post('/links/:id/:collection_id', (req, res) => {
-
+    var link_id = req.params.id;
+    var collection_id = req.params.collection_id;
+    utils.addLinkToCollection('url_info', link_id, collection_id, res);
 });
 
 //get all collections
 app.get('/collections', (req, res) => {
-
+    utils.getAll('collection', res);
 });
 
 //add new collection
 app.post('/collections', (req, res) => {
     var name = req.body.name;
-    pool.query({
-        text: `SELECT * FROM collection WHERE name = $1`,
-        values: [name]
-    }).then((result) => {
-        if(result.rowCount > 0){
-            res.send({
-                success: true,
-                message: 'Name has been use. Try other name. Success just means I got the request'
-            });
-            return false;   //not adding to database
-        }
-        return true;
-    }).then((result) => {
-        if(result){
-            pool.query({
-                text: `INSERT INTO collection(name) VALUES($1)`,
-                values: [name]
-            }).then((result) => {
-                console.log('Add new collection succefully');
-                res.send({
-                    success: true,
-                    rowCount: result.rowCount,
-                    rows: result.rows
-                });
-            }).catch((err) => {
-                console.log('Fail to add new collection');
-                res.send({
-                    success: false,
-                    error: err
-                });
-            });
-        }
-    }).catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        });
-    });
+    utils.postCollection('collection', name, res);
 });
 
 //get info of 1 collection
 app.get('/collections/:id', (req, res) => {
-
+    var id = req.params.id;
+    utils.getById('collection', id, res);
 });
 
 //update info of 1 collection
@@ -236,15 +83,17 @@ app.put('/collections/:id', (req, res) => {
 
 //delete collection
 app.delete('/collections/:id', (req, res) => {
-
+    var id = req.params.id;
+    utils.deleteById('collection', id, res);
 });
 
 //get all links in 1 collection
-app.get('/collections/:collection_id', (req, res) => {
-
+app.get('/collections/all/:collection_id', (req, res) => {
+    var collection_id = req.params.collection_id;
+    utils.getAllLinksOfCollection('url_info', collection_id, res);
 });
 
-//delete 1 link from collection
+//remove 1 link from a collection by set its collection_id to 1
 app.delete('/collections/:collection_id/:link_id', (req, res) => {
 
 });
--- utils/server_utils.js
+++ utils/server_utils.js
@@ -1,5 +1,15 @@
-const pg = require('pg');
+const { Pool } = require('pg');
 const dotenv = require('dotenv');
+const extractDomain = require('extract-domain');
+const got = require('got');
+const metascraper = require('metascraper')([
+    require('metascraper-description')(),
+    require('metascraper-image')(),
+    require('metascraper-logo')(),
+    require('metascraper-publisher')(),
+    require('metascraper-title')(),
+    require('metascraper-url')()
+]);
 
 dotenv.config();
 
@@ -7,37 +17,226 @@ const pool = new Pool({
     connectionString: process.env.LOCAL_DB
 });
 
-var checkExist = (table, column, value) => {
-    //check whether url already exists in the database
-    await pool.query({
-        text: `SELECT * FROM url_info WHERE url = $1`,
-        values: [link.url]
+var postLink = async (targetUrl, domain_table, url_table, res) => {
+    try {
+        //get information from url
+        var link = await got(targetUrl);
+        var link_info = await metascraper({ html: link.body, url: link.url });
+        //get information from domain
+        var domain = await got(extractDomain(targetUrl));
+        var domain_info = await metascraper({ html: domain.body, url: domain.url });
+
+        //add domain to database
+        await pool.query({
+            text: `SELECT * FROM ${domain_table} WHERE name = $1`,
+            values: [domain_info.url]
+        }).then((result) => {
+            if (result.rowCount !== 0) {
+                console.log('Domain already in the database');
+                return false;   //stop insert
+            }
+            return true;   //insert
+        }).then((result) => {
+            if (result) {
+                pool.query({
+                    text: `INSERT INTO 
+                        ${domain_table}(name, title, logo_link, img_link, description, publisher)
+                        VALUES($1, $2, $3, $4, $5, $6)`,
+                    values: [domain_info.url, domain_info.title, domain_info.logo, domain_info.image,
+                    domain_info.description, domain_info.publisher]
+                });
+            }
+        }).catch((err) => {
+            console.log('Fail to insert domain\n', err);
+        });
+
+        //get id of domain
+        var domain_id;
+        await pool.query({
+            text: `SELECT id FROM ${domain_table} WHERE name = $1`,
+            values: [domain_info.url]
+        }).then((result) => {
+            domain_id = result.rows[0].id;
+        }).catch((err) => {
+            console.log('Can not get domain id\n', err);
+        });
+
+        //check whether url already exists in the database
+        await pool.query({
+            text: `SELECT * FROM ${url_table} WHERE url = $1`,
+            values: [link.url]
+        }).then((result) => {
+            if (result.rowCount !== 0) {
+                console.log('Url exists in the database');
+                res.send({
+                    success: true,
+                    exists: true
+                });
+                return false;   //stop insert
+            }
+            return true;    //do insert into table
+        }).then((result) => {
+            if (result) {
+                pool.query({
+                    text: `INSERT INTO 
+                                ${url_table}(url, title, logo_link, img_link, description, publisher, collection_id, domain_id)
+                                VALUES($1, $2, $3, $4, $5, $6, $7, $8)`,
+                    values: [link.url, link_info.title, link_info.logo, link_info.image, link_info.description,
+                    link_info.publisher, 1, domain_id]
+                }).then(() => {
+                    res.send(link_info);
+                }).catch((err) => {
+                    res.send({ success: false, error: err });
+                });
+            }
+        }).catch((err) => {
+            console.log('Fail in inserting url to database');
+            return res.send({ success: false, error: err });
+        });
+    } catch (err) {
+        console.log('Catch exception\n', err);
+        return res.send({ success: false, error: err });
+    }
+};
+
+var postCollection = (table, name, res) => {
+    pool.query({
+        text: `SELECT * FROM ${table} WHERE name = $1`,
+        values: [name]
     }).then((result) => {
-        if (result.rowCount !== 0) {
-            console.log('Url exists in the database');
+        if (result.rowCount > 0) {
             res.send({
                 success: true,
-                exists: true
+                message: 'Name has been use. Try other name. Success just means I got the request'
             });
-            return false;   //stop insert
+            return false;   //not adding to database
         }
-        return true;    //do insert into table
+        return true;
     }).then((result) => {
-        if(result){
+        if (result) {
             pool.query({
-                text: `INSERT INTO 
-                        url_info(url, title, logo_link, img_link, description, publisher)
-                        VALUES($1, $2, $3, $4, $5, $6)`,
-                values: [link.url, link_info.title, link_info.logo, link_info.image, link_info.description,
-                link_info.publisher]
-            }).then(() => {
-                res.send(link_info);
+                text: `INSERT INTO ${table}(name) VALUES($1)`,
+                values: [name]
+            }).then((result) => {
+                console.log('Add new collection succefully');
+                res.send({
+                    success: true,
+                    rowCount: result.rowCount,
+                    rows: result.rows
+                });
             }).catch((err) => {
-                res.send({success: false, error: err});
+                console.log('Fail to add new collection');
+                res.send({
+                    success: false,
+                    error: err
+                });
             });
         }
     }).catch((err) => {
-        return res.send({success: false, error: err});
+        console.log('Add new collection occur exception\n', err);
+        res.send({
+            success: false,
+            error: err
+        });
+    });
+}
+
+var getAll = (table, res) => {
+    pool.query({
+        text: `SELECT * FROM ${table}`
+    }).then((result) => {
+        res.send({
+            rowCount: result.rowCount,
+            rows: result.rows
+        });
+    }).catch((err) => {
+        res.send({
+            success: false,
+            error: err
+        });
     });
 };
+
+var getById = (table, id, res) => {
+    pool.query({
+        text: `SELECT * FROM ${table} WHERE id = $1`,
+        values: [id]
+    }).then((result) => {
+        res.send({
+            rowCount: result.rowCount,
+            rows: result.rows
+        });
+    }).catch((err) => {
+        res.send({
+            success: false,
+            error: err
+        });
+    });
+};
+
+var deleteById = (table, id, res) => {
+    pool.query({
+        text: `DELETE FROM ${table} WHERE id = $1`,     //maybe need to add ON CASCADE
+        values: [id]
+    }).then((result) => {
+        res.send({
+            success: true,
+            result: result
+        });
+    }).catch((err) => {
+        console.log("Error in deleting row");
+        res.send({
+            success: false,
+            error: err
+        });
+    });
+};
+
+var getAllLinksOfCollection = (table, collection_id, res) => {
+    pool.query({
+        text: `SELECT * FROM ${table} WHERE collection_id = $1`,
+        values: [collection_id]
+    }).then((result) => {
+        if (result.rowCount == 0) {
+            console.log('There is no links in this collection');
+            res.send({
+                success: false,
+                message: 'There is no links in this collection'
+            });
+        } else {
+            res.send({
+                success: true,
+                rowCount: result.rowCount,
+                rows: result.rows
+            });
+        }
+    }).catch((err) => {
+        console.log('Catch an error\n', err);
+        res.send({
+            success: false,
+            error: err
+        });
+    });
+};
+
+var addLinkToCollection = (url_table, link_id, collection_id, res) => {
+    pool.query({
+        text: `UPDATE ${url_table} SET collection_id = $1 WHERE id = $2`,
+        values: [collection_id, link_id]
+    }).then((result) => {
+        res.send({
+            success: true,
+            message: 'Add succefully'
+        })
+    }).catch((err) => {
+        console.log('Fail to update collection');
+        res.send({
+            success: false,
+            error: err
+        });
+    });
+}
+
+
+module.exports = { postLink, postCollection, getAll, getById, deleteById, getAllLinksOfCollection, addLinkToCollection };
 
